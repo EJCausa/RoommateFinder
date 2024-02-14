b55d64cdb9362e2d1743b349438e79b844368329
Emmett5170 <79123321+Emmett5170@users.noreply.github.com>
2024-02-13 20:40:30 -0600

    Update App.java

--- Terminal Prototype/App.java
+++ Terminal Prototype/App.java
@@ -1,14 +1,13 @@
-import java.util.Scanner;
 
 public class App {
     
     public static void main(String[] args) throws Exception {
 
-        Scanner keyboard = new Scanner(System.in);
-
+        //displays the sign in page.
         Page ui = new Page();
         ui.signInPage();
 
         
     }
 }
+
