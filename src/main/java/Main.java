e2fe8aa4d1505d148ee8f99fb49bd1060c8aed01
mrshareholder <158225629+mrshareholder@users.noreply.github.com>
2024-03-26 16:28:52 -0500

    Still tiny main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,21 @@ public class Main {
     {
         //Controller declared here
         System.out.println("Roommate Finder initializing...");
+        boolean exitFlag = false; //once true, finish the program
+        String userInput = "";
+        Scanner keyboard = new Scanner(System.in);
+        while(!exitFlag)
+        {
+            //take user input from scanner
+            keyboard.nextLine().trim();
+            //convert to lowercase
+            userInput = userInput.toLowerCase();
+            if(userInput.equals("exit"))
+            {
+                exitFlag = true;
+            }
+            //potentially flush the scanner?
+        }
+
     }
 }
