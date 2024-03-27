528b82caf2f4c888c5a6902410e7e66928f06cbc
Emmett5170 <79123321+Emmett5170@users.noreply.github.com>
2024-03-27 16:28:36 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-public class Main {
+public class App {
+
     public static void main(String[] args)
     {
         //Controller declared here
@@ -7,20 +8,41 @@ public class Main {
         boolean exitFlag = false; //once true, finish the program
         String userInput = "";
         Scanner keyboard = new Scanner(System.in);
+
+
+        Controller pageController = new Controller();
+
         //Declare profile?
+
         while(!exitFlag)
         {
+            pageController.printStartupPage();
             //print prompt from controller
             //take user input from scanner
             userInput = keyboard.nextLine().trim();
             //convert to lowercase
             userInput = userInput.toLowerCase();
-            if(userInput.equals("exit"))
+            if(userInput.equals("/exit"))
             {
-                exitFlag = true;
+
+                while (!exitFlag) {
+                    pageController.printConfirmExitPage();
+                    userInput = keyboard.nextLine().trim();
+                    if (userInput.equals("no")) {
+                        break;
+                    } else if (userInput.equals("yes")) {
+                        exitFlag = true;
+                    }
+
+                }
+                
             }
             //potentially flush the scanner?
+            //I think we can flush the scanner in the controller methods
+            
         }
         System.out.println("Thank you for using the Roommate Finder!");
     }
+
+    
 }
