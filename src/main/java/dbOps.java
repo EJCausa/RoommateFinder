b674678d94f3a97e022ed3582be21a46091c7af9
mrshareholder <158225629+mrshareholder@users.noreply.github.com>
2024-04-02 15:41:05 -0500

    Microclass dbOps.java

---
+++ src/main/java/dbOps.java
@@ -0,0 +1,6 @@
+public class dbOps
+{
+  //we need to research how to make a database
+  //and then put methods to handle operations pulling data from the db here
+  //as well as operations pushing data to the db
+}
