ceca561f945085cb90130bd23c4a04a229309349
Nhcarro <112598740+Nhcarro@users.noreply.github.com>
2024-04-30 15:38:35 -0500

    Update README.md

--- src/main/java/ProfileController.java
+++ src/main/java/ProfileController.java
@@ -1,13 +1,15 @@
 public class ProfileController {
     private static ProfileHandler ProfileHandlerOBJ = new ProfileHandler();
-    private ProfileOBJ = new Profile();
+
     public static boolean signUp(String user,String email,String phone,String pass){
 
         ProfileHandlerOBJ.signUp(user, email, phone, pass);
         return true;
 
     }
-    public static profile login(String user, String email){
+    public static Profile login(String user, String email){
+
+
 
     }
 
